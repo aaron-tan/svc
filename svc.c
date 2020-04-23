03490127f475256905a77e8af118ef9355ffb550
Aaron <htan6081@uni.sydney.edu.au>
2020-04-23 20:55:22 +1000

    Implemented hash files function

--- svc.c
+++ svc.c
@@ -9,9 +9,43 @@ void cleanup(void *helper) {
     // TODO: Implement
 }
 
+// Calculate the hash value of a file.
 int hash_file(void *helper, char *file_path) {
-    // TODO: Implement
-    return 0;
+    if (file_path == NULL) {
+      return -1;
+    }
+
+    FILE* fp = fopen(file_path, "r");
+
+    // Check if the file exists.
+    if (fp == NULL) {
+      return -2;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+
+    // Get the num_bytes.
+    int num_bytes = ftell(fp);
+
+    // Reset the file.
+    fseek(fp, 0, SEEK_SET);
+
+    int hash = 0;
+
+    // Calculate hash of file name.
+    for (int i = 0; i < strlen(file_path); i++) {
+      hash = (hash + file_path[i]) % 1000;
+    }
+
+    // Calculate the hash of file contents.
+    for (int i = 0; i < num_bytes; i++) {
+      int byte = fgetc(fp);
+      hash = (hash + byte) % 2000000000;
+    }
+
+    fclose(fp);
+
+    return hash;
 }
 
 char *svc_commit(void *helper, char *message) {
@@ -67,4 +101,3 @@ char *svc_merge(void *helper, char *branch_name, struct resolution *resolutions,
     // TODO: Implement
     return NULL;
 }
-
